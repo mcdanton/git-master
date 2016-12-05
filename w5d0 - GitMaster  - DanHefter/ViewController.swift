22a5e09a2a8f8c2fcb8c27518deba1903d324852
mcdanton <danhefter@gmail.com>
2016-12-05 14:28:52 -0500

    Added button and label

--- w5d0 - GitMaster  - DanHefter/ViewController.swift
+++ w5d0 - GitMaster  - DanHefter/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+   
+   @IBOutlet weak var textLabel: UILabel!
+   @IBAction func changeLabelButton(_ sender: Any) {
+      textLabel.text = "Hooray I'm Free!"
+   }
 
+   
+   
    override func viewDidLoad() {
       super.viewDidLoad()
       // Do any additional setup after loading the view, typically from a nib.
